eef84be21ed2734b2b5907d608d4a734bc89cb48
Sanidhya <60773590+sanidhya-31@users.noreply.github.com>
2022-05-30 00:15:32 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -84,7 +84,7 @@ search.addEventListener("input", function(){
 /*
 Further Features:
 1. Add Title
-2. Mark a note as Important
+2. Mark a note as Important 
 3. Separate notes by user
 4. Sync and host to web server 
 */ 
